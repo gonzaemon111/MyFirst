d2c92ac5b128425c248730cfb64dcf60bef82367
GokiSakuraba <goki14914@icloud.com>
2018-12-12 21:40:45 +0900

    add programm for label

--- MyFirst/ViewController.swift
+++ MyFirst/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBOutlet weak var outputLabel: UILabel! // これがLabelとプログラムの関連付けコード
+    
 }
 
